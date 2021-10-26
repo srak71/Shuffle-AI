c80474dc0d60dccb090cbd468e1e0428fec0c560
Saransh Rakshak <srakshak@berkeley.edu>
2021-10-25 20:06:37 -0700

    finished audio convert files

---
+++ lyric_analysis.py
@@ -0,0 +1,52 @@
+"""
+AI for Music Shuffle Optimization:
+Mood Association and Cognitive Response
+in Individuals to Auditory Stimulation
+@saranshrakshak
+Oct-24-2021
+v1.0.4
+https://github.com/saranshrakshak/shuffle_optim_AI.git
+"""
+from pydub import AudioSegment
+import speech_recognition as sr
+import os
+from nlkt.sentiment.vader import SentimentIntensityAnalyzer
+
+## changing format of incoming file from .m4a (or other) to .wav
+# returns an AudioSegment object
+def convert_to_wav(filename):
+    filename = 'audio_files/' + filename
+    file_type = filename.split(".")[1]
+
+    if file_type != '.wav':
+        filename.split(".")[0] + ".wav"
+        audio = AudioSegment.from_file(filename)
+        audio.export(new_name, format = 'wav')
+        return new_name
+    else:
+        if os.path.exists(filename,__contains__('.wav')):
+            # print('File <<', filename, '>> already converted to wav and removed.')
+            return filename.split(".")[0] + '.wav'
+        else:
+            # print('File <<', filename, '>> non-existant.')
+            return filename.split(".")[0] + '.wav'
+
+# get the lyrics to our song file
+def transcribe_audio(wav_file):
+    if not wav_file: return False
+    recognizer = sr.Recognizer()
+    # Import the audio file and get song lyrics
+    audio_file = sr.AudioFile(wav_file)
+    with audio_file as source:
+        audio_data = recognizer.record(source)
+    return recognizer.recognize_google(audio_data)
+
+# get sentiment of lyrics
+def analyze_text(text_file):
+    if not text_file: return False
+    intensity = SentimentIntensityAnalyzer().polarity_score(text_file)
+    return SentimentIntensityAnalyzer
+
+# get volume user is listening song to
+def get_volume():
+    return AudioSegment.from_file(filename).dBFS
---
+++ module_1.py
@@ -0,0 +1,2 @@
+import numpy as np
+from lyric_analysis import *
